5c56544fd719551b7ae124b87df4d2e5f8189f60
VladislavKostylev <MoonInvader@yandex.ru>
2024-06-21 11:43:28 +0300

    Initial Commit

---
+++ App_ToDoList/App_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ToDoListApp.swift
+//  App_ToDoList
+//
+//  Created by Владислав on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct App_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
